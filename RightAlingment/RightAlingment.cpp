d074800e250da44f870bbb0971776aa33f2d177f
VlAdOn822 <72297860+VlAdOn822@users.noreply.github.com>
2022-05-13 22:46:23 +0200

    Alingment without wordwrap

---
+++ RightAlingment/RightAlingment.cpp
@@ -0,0 +1,44 @@
+// RightAlingment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+	int lOfLine;
+	cin >> lOfLine;
+	int row = 1;
+	string currentWord;
+	for (int i = 0; i <= s.length(); i++)
+	{
+		if (i <= lOfLine * row && s[i] != ' ' && i != s.length())
+		{
+			currentWord += s[i];
+		}
+		else if (i == s.length() || i <= lOfLine * row && s[i] == ' ')
+		{
+			cout << currentWord << " ";
+			currentWord = "";
+		}
+		else
+		{
+			cout << endl;
+			row++;
+		}
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
